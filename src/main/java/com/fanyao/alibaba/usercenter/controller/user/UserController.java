b72316f57acb4f8b61c57087b0643c78e9cfe20e
fanyao <fanyao931213>
2020-03-17 16:16:17 +0800

    添加配置文件

--- src/main/java/com/fanyao/alibaba/usercenter/controller/user/UserController.java
+++ src/main/java/com/fanyao/alibaba/usercenter/controller/user/UserController.java
@@ -5,10 +5,7 @@ import com.fanyao.alibaba.usercenter.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author: bugProvider
@@ -26,6 +23,17 @@ public class UserController {
     public User findById(@PathVariable Integer id){
         log.info("接收GET请求");
         return userService.findById(id);
+    }
+
+    // q=?id=xxx&wxid=xxx&...
+    @GetMapping("/q")
+    public User query(User user){
+        return user;
+    }
+
+    @PostMapping("/post")
+    public User post(@RequestBody User user) {
+        return user;
     }
 
 }
